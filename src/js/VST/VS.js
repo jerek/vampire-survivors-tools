17506730a105c71a24d88ba80aacb1817edb52e9
Jerek Dain <jerekdain@gmail.com>
2022-07-05 12:31:31 -0500

    add passive item image filenames

--- src/js/VST/VS.js
+++ src/js/VST/VS.js
@@ -35,6 +35,7 @@ VST.VS = new function () {
     /**
      * @typedef {Object} PassiveData Data describing a passive item.
      * @property {string}    description
+     * @property {string}    frameName   The filename of the passive item's image within the "items" sprite.
      * @property {PassiveId} id          This MUST NOT change, because it's used in the URL for saved builds.
      * @property {string}    name
      * @property {number}    order       The order that this passive item is displayed in the in-game Collection.
@@ -485,106 +486,127 @@ VST.VS = new function () {
         [PASSIVE_ID_SPINACH]: {
             name: 'Spinach',
             description: 'Raises inflicted damage by 10%.',
+            frameName: 'Leaf.png',
             order: 1,
         },
         [PASSIVE_ID_ARMOR]: {
             name: 'Armor',
             description: 'Reduces incoming damage by 1. Increases retaliatory damage by 10%.',
+            frameName: 'ArmorIron.png',
             order: 2,
         },
         [PASSIVE_ID_HOLLOW_HEART]: {
             name: 'Hollow Heart',
             description: 'Augments max health by 20%.',
+            frameName: 'HeartBlack.png',
             order: 3,
         },
         [PASSIVE_ID_PUMMAROLA]: {
             name: 'Pummarola',
             description: 'Character recovers 0.2 HP per second.',
+            frameName: 'HeartRuby.png',
             order: 4,
         },
         [PASSIVE_ID_EMPTY_TOME]: {
             name: 'Empty Tome',
             description: 'Reduces weapons cooldown by 8%.',
+            frameName: 'Book2.png',
             order: 5,
         },
         [PASSIVE_ID_CANDELABRADOR]: {
             name: 'Candelabrador',
             description: 'Augments area of attacks by 10%.',
+            frameName: 'Candelabra.png',
             order: 6,
         },
         [PASSIVE_ID_BRACER]: {
             name: 'Bracer',
             description: 'Increases projectiles speed by 10%.',
+            frameName: 'Gauntlet.png',
             order: 7,
         },
         [PASSIVE_ID_SPELLBINDER]: {
             name: 'Spellbinder',
             description: 'Increases duration of weapon effects by 10%.',
+            frameName: 'EmblemEye.png',
             order: 8,
         },
         [PASSIVE_ID_DUPLICATOR]: {
             name: 'Duplicator',
             description: 'Weapons fire more projectiles.',
+            frameName: 'Ring.png',
             order: 9,
         },
         [PASSIVE_ID_WINGS]: {
             name: 'Wings',
             description: 'Character moves 10% faster.',
+            frameName: 'Wing.png',
             order: 10,
         },
         [PASSIVE_ID_ATTRACTORB]: {
             name: 'Attractorb',
             description: 'Character pickups items from further away.',
+            frameName: 'OrbGlow.png',
             order: 11,
         },
         [PASSIVE_ID_CLOVER]: {
             name: 'Clover',
             description: 'Character gets 10% luckier.',
+            frameName: 'Clover.png',
             order: 12,
         },
         [PASSIVE_ID_CROWN]: {
             name: 'Crown',
             description: 'Character gains 8% more experience.',
+            frameName: 'Crown.png',
             order: 13,
         },
         [PASSIVE_ID_STONE_MASK]: {
             name: 'Stone Mask',
             description: 'Character earns 10% more coins.',
+            frameName: 'Mask.png',
             order: 14,
         },
         [PASSIVE_ID_SKULL_O_MANIAC]: {
             name: 'Skull O\'Maniac',
             description: 'Increases enemy speed, health, quantity, and frequency by 10%.',
+            frameName: 'Curse.png',
             order: 15,
         },
         [PASSIVE_ID_TIRAGISU]: {
             name: 'Tiragisú',
             description: 'Revives once with 50% health.',
+            frameName: 'Tiramisu.png',
             order: 16,
         },
         [PASSIVE_ID_SILVER_RING]: {
             name: 'Silver Ring',
             description: 'Wear ... Clock ...',
+            frameName: 'silverring.png',
             order: 17,
         },
         [PASSIVE_ID_GOLD_RING]: {
             name: 'Gold Ring',
             description: '... With ... Lancet',
+            frameName: 'goldring.png',
             order: 18,
         },
         [PASSIVE_ID_METAGLIO_LEFT]: {
             name: 'Metaglio Left',
             description: 'Channels dark powers to protect the bearer.',
+            frameName: 'bsleft.png',
             order: 19,
         },
         [PASSIVE_ID_METAGLIO_RIGHT]: {
             name: 'Metaglio Right',
             description: 'Channels dark powers to curse the bearer.',
+            frameName: 'bsright.png',
             order: 20,
         },
         [PASSIVE_ID_TORRONAS_BOX]: {
             name: 'Torrona\'s Box',
             description: 'Cursed item, but increases Might, Projectile Speed, Duration, and Area by 4%.',
+            frameName: 'torrone.png',
             order: 21,
         },
     };
